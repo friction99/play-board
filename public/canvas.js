00ca12e0367459d9daca5f42013eb602040fb96d
Aman Kumar <amankun77@gmail.com>
2026-05-02 02:13:27 +0530

    redis added

--- public/canvas.js
+++ public/canvas.js
@@ -1,4 +1,26 @@
 const socket = io()
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+let drawing = false;
+let prev= {x:0,y:0};
+
+//Draw a single stroke segment
+function drawStroke({prevX,prevY,x,y,color,width}){
+    ctx.beginPath();
+    ctx.moveTo(prevX,prevY);
+    ctx.lineTo(x,y);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.lineCap = 'round';
+    ctx.stroke();
+}
+//Socket events 
+socket.on('init',(strokes)=>{
+    strokes.forEach(drawStroke);
+});
+//Recieve strokes from other users
+socket.on('draw',drawStroke);
 
 socket.on('connect',()=>{
     console.log("Connected with ID:",socket.id);
@@ -11,3 +33,31 @@ socket.on('user-count',(count)=>{
 socket.on('disconnect',()=>{
     console.log('Disconnected');
 })
+
+//Mouse events
+
+canvas.addEventListener('mousedown',(e)=>{
+    drawing = true;
+    prev = { x: e.offsetX, y: e.offsetY };
+})
+
+canvas.addEventListener('mousemove',(e)=>{
+    if(!drawing) return;
+
+    const data = {
+        prevX: prev.x,
+        prevY: prev.y,
+        x: e.offsetX,
+        y: e.offsetY,
+        color: '#000000',
+        width: 3,
+        userId: socket.id
+    }
+
+    drawStroke(data);
+    socket.emit('draw',data);
+    prev = {x:data.x,y:data.y};
+})
+
+canvas.addEventListener('mouseup',()=> drawing = false);
+canvas.addEventListener('mouseleave',()=>drawing = false);
--- src/sockets/boardHandler.ts
+++ src/sockets/boardHandler.ts
@@ -1,12 +1,18 @@
 import {Server,Socket} from 'socket.io'
 import type {DrawEvent} from "../types/board.js";
+import redis from '../configs/redisClient.js';
 const strokes: DrawEvent[] = [];
-export function boardHandler(io:Server,socket:Socket){
+const BOARD_KEY = 'board:strokes';
+export async function boardHandler(io:Server,socket:Socket){
     console.log("User connected",socket.id);
+    //Load strokes from redis and send to new joiner
+    const saved = await redis.get(BOARD_KEY);
+    const strokes: DrawEvent[] = saved ? JSON.parse(saved): [];
     socket.emit('init',strokes);
     io.emit("user-count",io.engine.clientsCount);
-    socket.on('draw',(data:DrawEvent)=>{
+    socket.on('draw',async (data:DrawEvent)=>{
         strokes.push(data); //store in memory
+        await redis.set(BOARD_KEY,JSON.stringify(strokes));
         socket.broadcast.emit('draw',data);//send to everyone except sender
     })
     socket.on('disconnect',()=>{
